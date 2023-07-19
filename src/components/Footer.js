7fcfae2aac7f3013e4ad166270a31c19bfea9d82
misael <misaelucas@gmail.com>
2023-07-19 00:12:32 -0300

    fixed footer error with color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ export const Footer = () => {
   const projectName = "surfing at the internetz";
 
   return (
-    <div className="flex flex-wrap justify-center underline underline-offset-4 text-sm">
+    <div className="flex flex-wrap justify-center text-sm">
       <div className="flex my-2 mt-3">
-        <a href="https://leekspin.com/" className="rainbow mr-1">
+        <a href="https://leekspin.com/" className="rainbow underline underline-offset-4 mr-1 decoration-white">
           {projectName}
         </a>
-        <span className="ml-1 nounder text-white"> -</span>
+        <span className="ml-1 text-white">-</span>
         <a
-          className="ml-2 decoration-white hover:text-pink-500"
+          className="ml-2 underline underline-offset-4 decoration-white hover:text-pink-500"
           href="https://github.com/misaelucas"
         >
           misa
