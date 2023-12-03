8b49d6a63e0423df3612cae369572c1b97d34c7c
misaelucas <misaelucas@gmail.com>
2023-12-03 12:18:24 -0300

    fixing the lack of content being included in the tailwind.config.js"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], theme: {
     extend: {},
     fontSize: {
       sm: "0.8rem",
