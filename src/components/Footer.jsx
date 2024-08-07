b87f4b978c4ac3a173f2f9230be4c22827cd28fe
misaelucas <misaelucas@gmail.com>
2024-08-06 23:57:35 -0300

    Footer refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,21 @@
 import { BsGithub } from "react-icons/bs";
 
-export const Footer = () => {
+ function Footer ()  {
   return (
-    <div className="flex justify-center">
-      <div className="text-xl mb-2">
+    <div className="flex justify-center !font-mono mb-2">
+      <div className="text-xl ">
         <a
-          className="underline underline-offset-4 decoration-white hover:text-pink-500"
+          className="underline underline-offset-4 text-2xl decoration-white hover:text-pink-500"
           href="https://github.com/misaelucas"
         >
           misaelucas
         </a>
-        <a className="ml-4 pt-12" href="https://github.com/misaelucas">
-          <BsGithub style={{ fontSize: '1.5em', }} />
+        <a className="ml-4 -mb-2" href="https://github.com/misaelucas">
+          <BsGithub style={{ fontSize: '1.4em', }} />
         </a>
       </div>
     </div>
   );
 };
+
+export default Footer
