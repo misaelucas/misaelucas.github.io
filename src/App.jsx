6e81f7fd479e2529ddbf36d6b9916d122276fa82
misaelucas <misaelucas@gmail.com>
2024-08-06 23:57:35 -0300

    Footer refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
 import MainPage from "./components/MainPage";
+import Footer from "./components/Footer";
 
 function App() {
   console.log(
     "Hallo =)."
   );
   return (
-    <div className="App fade-in overflow-hidden">
+    <div className="App overflow-hidden">
       <Header />
       <MainPage />
+      <Footer />
+
     </div>
   );
 }
